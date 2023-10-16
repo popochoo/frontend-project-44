43341c6559761187e3d53662547f788a70266ded
Timur <polshamangood@gmail.com>
2023-10-15 19:24:12 -0700

    edits in files (error corrections)

--- Makefile
+++ Makefile
@@ -4,9 +4,6 @@ install:
 brain-games:
 	node bin/brain-games.js
 
-brain-even:
-	node bin/brain-even.js	
-
 publish:
 	npm publish --dry-run
 
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
@@ -5,42 +6,34 @@ const username = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${username}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-let count = 0
+let count = 0;
 
 while (count < 3) {
-    const number = Math.round(Math.random() * 101);
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
+  const number = Math.round(Math.random() * 101);
+  console.log(`Question: ${number}`);
+  const answer = readlineSync.question('Your answer: ');
 
-    if (number % 2 === 0 && answer === 'yes') {
+if (number % 2 === 0 && answer === 'yes') {
         console.log('Correct!');
-        count += 1
-    } 
-    
-    else if (number % 2 !== 0 && answer === 'no') {
+        count += 1;
+    } else if (number % 2 !== 0 && answer === 'no') {
         console.log('Correct!');
-        count += 1
-    } 
-    
-    else if (number % 2 === 0 && answer === 'no') {
+        count += 1;
+    } else if (number % 2 === 0 && answer === 'no') {
         console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
         console.log(`Let's try again, ${username}!`);
-        break
-    }
-    
-    else if (number % 2 !== 0 && answer === 'yes') {
+        break;
+    } else if (number % 2 !== 0 && answer === 'yes') {
         console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
         console.log(`Let's try again, ${username}!`);
-        break
-    }
-
-    else {
+        break;
+    } else {
         console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
         console.log(`Let's try again, ${username}!`);
-        break
-    };
+        break;
+    }
 };
 
 if (count === 3) {
-    console.log(`Congratulations, ${username}!`)
+    console.log(`Congratulations, ${username}!`);
 };
