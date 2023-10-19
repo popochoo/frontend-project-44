f271efededd4c65d3ad3c2aaf37e6bdd1d36a07f
Timur <polshamangood@gmail.com>
2023-10-18 17:45:36 -0700

    create brain-gcd-date.js

---
+++ src/games/brain-gcd-date.js
@@ -0,0 +1,30 @@
+import baseLogic from "../index.js";
+
+const dataPreparationBrainGcd = () => {
+    const correctAnswer = (numbers) => {
+        const listNumbers = numbers.split(' ');
+        let x = Number(listNumbers[0]);
+        let y = Number(listNumbers[1]);
+    
+        while (x !== y) {
+            if (x > y) {
+                x -= y;
+            } else {
+                y -= x;
+            };
+        };
+    
+        return x.toString();
+    };
+
+    const randomQuestion = () => {
+        return `${Math.round(Math.random() * 101)} ${Math.round(Math.random() * 101)}`
+    }
+
+    const rules = 'Find the greatest common divisor of given numbers.';
+
+    baseLogic(rules, randomQuestion, correctAnswer);
+};
+
+
+dataPreparationBrainGcd();
