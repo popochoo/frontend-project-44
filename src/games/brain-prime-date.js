227ef8ca7838f357324ee5793a949f74c4d2e5eb
Timur <polshamangood@gmail.com>
2023-10-21 00:37:02 -0700

    small fixing

--- src/games/brain-prime-date.js
+++ src/games/brain-prime-date.js
@@ -2,14 +2,14 @@ import baseLogic from '../index.js';
 
 const dataPreparationBrainPrime = () => {
   const correctAnswer = (number) => {
-    let answer = '';
-    if (number > 1) {
+    let answer = 'yes';
+    if (number !== 1) {
       for (let i = 2; i < number; i += 1) {
         if (number % i === 0) {
           answer = 'no';
+          break;
         }
       }
-      answer = 'yes';
     }
 
     return answer;
