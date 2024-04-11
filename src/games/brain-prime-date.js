d97ef0d283644a49f6345e16c4d7aef0e2020f89
popochoo <polshamangood@gmail.com>
2024-04-11 19:23:55 +0300

    update brain-prime-date.js

--- src/games/brain-prime-date.js
+++ src/games/brain-prime-date.js
@@ -2,16 +2,13 @@ import baseLogic from '../index.js';
 
 const dataPreparationBrainPrime = () => {
   const correctAnswer = (number) => {
-    let answer = 'yes';
-    if (number !== 1) {
-      for (let i = 2; i < number; i += 1) {
-        if (number % i === 0) {
-          answer = 'no';
-        }
-      }
-    }
-
-    return answer;
+    const primeNumbers = [
+      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+      43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    ];
+    const isPrime = primeNumbers.includes(number);
+    const rightAnswer = isPrime ? 'yes' : 'no';
+    return rightAnswer;
   };
 
   const randomQuestion = () => Math.round(Math.random() * 10);
